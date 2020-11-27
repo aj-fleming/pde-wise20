5de6f686024d8aceed9ca2ff94d9f8b3ac0403a1
Catalin <catalin.dascaliuc96@gmail.com>
2020-11-27 12:43:27 +0100

    add fix for rhs dirac, such that it works even when there is a grid point at x=1/2

--- prog1/assemble_rhs_dirac.m
+++ prog1/assemble_rhs_dirac.m
@@ -6,7 +6,7 @@ function [b] = assemble_rhs_dirac(gp, N)
   for j=1:N
     if 0.5 < gp(j+1) && 0.5 > gp(j)
       b(j,1) = 2*(0.5-gp(j))/(gp(j+1)-gp(j));
-    elseif 0.5 < gp(j+2) && 0.5 > gp(j+1)
+    elseif 0.5 < gp(j+2) && 0.5 >= gp(j+1)
       b(j,1) = 2*(gp(j+2)-0.5)/(gp(j+2)-gp(j+1));
     end
   end
