3f2f33ece2a320582d4d62e8d82799f79197a0c6
Catalin <catalin.dascaliuc96@gmail.com>
2021-01-16 18:43:50 +0100

    Add comments for code legibility;

--- prog3/Visualize.m
+++ prog3/Visualize.m
@@ -1,18 +1,21 @@
-N = 1000;
-T = 5;
-u0_interval = [-1, 1];
-x_v = linspace(u0_interval(1),u0_interval(2),N);
-% fun = @(x) 1.*(( x>=-1 && x<-1/2) || ( x>=1/2 && x<1));
-fun = @(x) sin(pi*x);
-u = solve_pde(N,T,u0_interval,fun,'LF');
+N = 500; % grid points
+T = 1; %Time
+dt = 0; % time interval; specify it as 0 for the default value to be set based on CFL condition
+u0_interval = [-1, 1]; % the domain interval
+x_v = linspace(u0_interval(1),u0_interval(2),N); 
+% function handle of initial condition pieceiwise function
+u0_fun = @(x) 1.*(( x>=-1 && x<-1/2) || ( x>=1/2 && x<=1)); 
+% u0_fun = @(x) sin(pi*x); % function handle of initial condition sin(pi x)
+F_type = 'LW'; % flux type options: 'naive', 'LF', 'LW'
+u = solve_pde(N,T,dt,u0_interval,u0_fun,F_type); % solve the pde
+% animated plot
 figure
-plot(x_v,u(1,:));
-xlim([u0_interval(1) u0_interval(2)])
-ylim([0,1])
-
+ax1 = axes;
+xlim(ax1,[u0_interval(1) u0_interval(2)])
+ylim(ax1,[0,1])
+size_max = size(u,1);
 for idx = 2:size(u,1)
-    plot(x_v,u(idx,:));
+    plot(ax1,x_v,u(idx,:));
+    title(ax1,['T = ', num2str(idx/size_max*T)]);
     pause(0.001);
-    xlim([u0_interval(1) u0_interval(2)])
-    ylim([0,1])
 end
--- prog3/solve_pde.m
+++ prog3/solve_pde.m
@@ -1,30 +1,48 @@
-function u = solve_pde(N,T,u0_interval,u0,F_type)
-% u0 - matrix size: n by 3; n number of intervals; 1st value left bound,
-% 2nd value right bound, 3rd value- value of function2
-f_type = 'advection';
-% initialize
-interval = linspace(u0_interval(1),u0_interval(2),N);
-dx = interval(2)-interval(1);
+function u = solve_pde(N,T,dt,u0_interval,u0,F_type)
+% Inputs:
+%       N - number of gridpoints in space domain
+%       T - end time 
+%       dt - time step; specify 0 to use default value based on CFL;
+%       u0_interval - domain of the space e.g: [-1 1]
+%       u0 - function handle of the initial condition
+%       F_type - numerical flux type; options: 'naive', 'LF', 'LW' 
+% Outputs:
+%       u - the solution in matrix form; each row is u(x) at specific time
+f_type = 'advection'; % type of flux function; 'advection' or 'burgers'
+% create the space discretization
+x_vec = linspace(u0_interval(1),u0_interval(2),N);
+dx = x_vec(2)-x_vec(1);
+% set dt according to CFL condition
 switch f_type
     case 'advection'
-        dt = dx/2;
+        if dt==0 || dt > dx/2
+            dt = dx/2;
+        end
     case 'burgers'
-        dt = dx/u0_interval(2);
+        if dt==0 || dt> dx/max(u0_interval) 
+            dt = dx/max(u0_interval);
+        end
 end
+% create time discretization vector
 t = 0:dt:T;
+% initialize u
 u = zeros(length(t),N);
+% set the initial conditions for u0 (aka u(1,:)) 
 for idx2 = 1:length(u(1,:))
-    u(1,idx2) = u0(interval(idx2));
+    u(1,idx2) = u0(x_vec(idx2));
 end
-u(1,end) = 1;
+% solve the pde for u at each j and n
 for n=1:size(u,1)
     for j=1:size(u,2)
         if j > 1 && j < size(u,2)
+            % solution for interior space points
             u(n+1,j) = u(n,j) + dt/dx*(F(u(n,j-1),u(n,j),dx,dt,F_type,f_type)-F(u(n,j),u(n,j+1),dx,dt,F_type,f_type));
         elseif j == 1
+            % solution for j==1; make it periodic (assign for ul = u(last))
             u(n+1,j) = u(n,j) + dt/dx*(F(u(n,end),u(n,j),dx,dt,F_type,f_type)-F(u(n,j),u(n,j+1),dx,dt,F_type,f_type));
            
         elseif j == size(u,2)
+            % solution at the right end of space discretization; (assign for ur = u(1))
             u(n+1,j) = u(n,j) + dt/dx*(F(u(n,j-1),u(n,j),dx,dt,F_type,f_type)-F(u(n,j),u(n,1),dx,dt,F_type,f_type));
         end
     end
@@ -32,13 +50,21 @@ end
 
 end
 function result = F(ul,ur,dx,dt,F_type,f_type)
+% Numerical flux function
+% Inputs:
+%       ul - u left
+%       ur - u right
+%       F_type - numerical flux function type; options: 'naive', 'LF', 'LW'
+%       f_type - flux function type; options: 'advection', 'burgers'
+% Outputs:
+%       resuls - scalar value F(ul,ur)
 switch F_type
     case 'naive'
         result = 1/2*(f(ul,f_type)+f(ur,f_type));
     case 'LF'
         result = 1/2*(f(ul,f_type)+f(ur,f_type))- dx/2/dt*(ur-ul);
     case 'LW'
-        if (ur-ul) == 0
+        if (ur-ul) == 0 % make sure there is no division by 0
             result = 1/2*(f(ul,f_type)+f(ur,f_type));
         else
             result = 1/2*(f(ul,f_type)+f(ur,f_type))- dt/2/dx*(ur-ul)*((f(ul,f_type)-f(ur,f_type))/(ul-ur))^2;
@@ -46,6 +72,7 @@ switch F_type
 end
 end
 function result = f(x,f_type)
+% flux function 
 switch f_type
     case 'advection'
         result = 2*x;
