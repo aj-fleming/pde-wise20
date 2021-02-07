7c3180a77cd308ad0552e4ee15178f508f2b1f3c
kenny-yung <kenny-yung@users.noreply.github.com>
2021-02-07 17:58:00 +0100

    changed to while loop

--- prog4/prog4_main.m
+++ prog4/prog4_main.m
@@ -1,23 +1,16 @@
-N = 200;
+N = 50; %grid cells
 x_start = 0;
 x_end = 1;
-T_end = 0.5;
+T_end = 0.25;
 dx = (x_end-x_start)/N;
 cfl_a = 0.5;
-dt = cfl_a*dx; %/max(abs(eig(Aj))); %CFL condition
-lambda = dt/dx;
-T = 0:dt:T_end;
-if T_end-T(end) > 0
-    T = [T, T_end];
-end
 x = linspace(x_start,x_end,N);
 u0 = zeros(3,N); %rho,v,P
-IC = '1'; %initial condition
+IC = '2'; %initial condition
 switch IC
     case '1'
         u0 = [1, 0, 1; %rho, v, p
               0.125, 0, 0.1];
-        
     case '2'
         u0 = [1, -1, 0.4;
               1, 1, 0.4];
@@ -26,7 +19,7 @@ U0 = cell(3,1);
 U0{1} = @(x) u0(1,1).* and(x>=0, x<=0.5) + u0(2,1).* and(x<=1, x>=0.5);
 U0{2} = @(x) u0(1,1)* u0(1,2).* and(x>=0, x<=0.5) + u0(2,1)*u0(2,2).* and(x<=1, x>=0.5);
 U0{3} = @(x) 0.5.*U0{1}(x).*U0{2}(x).^2 + u0(1,3)/(Cv-1).* and(x>=0, x<=0.5) + u0(2,3)/(Cv-1).* and(x<=1, x>=0.5);
-U = zeros(3,N,length(T));
+U = zeros(3,N,2);
 %calc cell mean value
 for i = 1:N
     U(1,i,1) =  integral(U0{1},x_start+(i-1)*dx, x_start+(i*dx))/dx;
@@ -34,20 +27,32 @@ for i = 1:N
     U(3,i,1) =  integral(U0{3},x_start+(i-1)*dx, x_start+(i*dx))/dx;
 end
 
-for n = 1:size(U,3)-1 %iterate over time
+time = 0; n = 0;
+while(true) %iterate over time
+    %loop
+    dt = cfl_a*dx/max(abs(eig(Aj))); %CFL condition
+    if n+dt > T_end
+        break;
+    end
+    lambda = dt/dx;
     %calc first one
-    U(:,1,n+1) = U(:,1,n)-dt/dx*(F_LF(U(:,1,n),U(:,2,n),lambda)-F_LF(U(:,1,n),U(:,1,n),lambda));
+    U(:,1,n+1) = U(:,1,n)-dt/dx*(F(U(:,1,n),U(:,2,n),lambda)-F(U(:,1,n),U(:,1,n),lambda));
+    %calc 2 thru N-1
     for j = 2:size(U,2)-1 %iterate over x
-        F_1 = F_LF(U(:,j,n),U(:,j+1,n),lambda);
-        F_2 = F_LF(U(:,j-1,n),U(:,j,n),lambda);
+        F_1 = F(U(:,j,n),U(:,j+1,n),lambda);
+        F_2 = F(U(:,j-1,n),U(:,j,n),lambda);
         U(:,j,n+1) = U(:,j,n)-dt/dx*(F_1-F_2);
     end
     %calc last one
     j = size(U,2);
-    U(:,j,n+1) = U(:,j,n)-dt/dx*(F_LF(U(:,j,n),U(:,j,n),lambda)-F_LF(U(:,j-1,n),U(:,j,n),lambda));
+    U(:,j,n+1) = U(:,j,n)-dt/dx*(F(U(:,j,n),U(:,j,n),lambda)-F(U(:,j-1,n),U(:,j,n),lambda));
+end
+if T_end-time > 0
+    %calc last time step
 end
 
 figure(1)
+set(gcf, [100 100 1000 600]);
 for t = 1:length(T)
     tiledlayout(3,1);
     nexttile
@@ -60,7 +65,7 @@ for t = 1:length(T)
     title("Velocity");
     nexttile
     plot(x,U(3,:,t));
-    xlabel("T = "+t);
+    xlabel("T = "+t*dt);
     title("Pressure");
     pause(0.005);
 end
@@ -68,7 +73,7 @@ end
 function F_LF = F_LF(uL,uR,lambda)
     F_LF = 0.5*(A(uL)*uL + A(uR)*uR - (uR-uL)/lambda);
 end
-function FLF = F(uL,uR) %u[3x1]
+function FLF = F(uL,uR,lambda) %u[3x1]
     FLF = zeros(3,1);
     aL = min(eig(A(uL)),eig(A(uR)));
     aR = max(abs(eig(A(uL))),abs(eig(A(uR))));
