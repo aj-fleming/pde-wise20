50eb2f2fa7267ead1a2fb52c8473653cc3363db1
Alexander Fleming <alexander.fleming@rwth-aachen.de>
2021-02-09 19:55:05 +0100

    correct octave/matlab implementation; figures plot incorrect values (density, momentum, energy), not (density, velocity, pressure)

--- prog4/prog4_main.m
+++ prog4/prog4_main.m
@@ -1,4 +1,4 @@
-N = 50; %grid cells
+N = 20; %grid cells
 x_start = 0;
 x_end = 1;
 T_end = 0.5;
@@ -16,24 +16,69 @@ switch IC
         u0 = [1, -1, 0.4;
               1, 1, 0.4];
 end
+
 U0 = cell(3,1);
 U0{1} = @(x) u0(1,1).* and(x>=0, x<=0.5) + u0(2,1).* and(x<=1, x>=0.5);
 U0{2} = @(x) u0(1,1)* u0(1,2).* and(x>=0, x<=0.5) + u0(2,1)*u0(2,2).* and(x<=1, x>=0.5);
 U0{3} = @(x) 0.5.*U0{1}(x).*U0{2}(x).^2 + u0(1,3)/(Cv-1).* and(x>=0, x<=0.5) + u0(2,3)/(Cv-1).* and(x<=1, x>=0.5);
+
 U = zeros(3,N,2);
-%calc cell mean value
 for i = 1:N
     U(1,i,1) =  integral(U0{1},x_start+(i-1)*dx, x_start+(i*dx))/dx;
     U(2,i,1) =  integral(U0{2},x_start+(i-1)*dx, x_start+(i*dx))/dx;
     U(3,i,1) =  integral(U0{3},x_start+(i-1)*dx, x_start+(i*dx))/dx;
 end
 
+function e = fast_eigs(u, heat_ratio)
+  v = u(2)/u(1);
+  P = (heat_ratio-1)*(u(3)-0.5*u(1)*v^2);
+  mach = sqrt(heat_ratio*u(1)/P);
+  e = [v-mach, v, v+mach];
+end
+
+% lax-friedrichs "cheat" flux to test/compare
+% takes two 3x1 arrays and the courant number
+%function F_LF = F_LF(uL,uR,lambda)
+%    F_LF = 0.5*(A(uL)*uL + A(uR)*uR - (uR-uL)/lambda);
+%end
+
+% HLL flux as defined in the lecture
+% accepts two 3x1 vectors and the heat capacity ratio (>1)
+function FLF = F_HLL(uL,uR,heat_ratio) %u[3x1]
+    FLF = zeros(3,1);
+    evals = [fast_eigs(uL, heat_ratio) fast_eigs(uR, heat_ratio)];
+    aL = min(evals);
+    aR = max(abs(evals));
+    if aL >= 0
+        FLF = f(uL,heat_ratio);
+    elseif (aL <= 0) & (aR >= 0)
+        FLF = 1./(aR-aL).*(aR.*f(uL,heat_ratio)-aL.*f(uR,heat_ratio)+aL.*aR.*(uR-uL));
+    elseif aR >= 0
+        FLF = f(uR,heat_ratio);
+    end
+end
+
+function Au = A(u, heat_ratio)
+    u1 = u(1);
+    u2 = u(2);
+    u3 = u(3);
+    Au = [0, 1, 0;
+          (heat_ratio-3)/2*(u2/u1)^2, (3-heat_ratio)*(u2/u1), heat_ratio-1;
+          -heat_ratio*u2*u3/(u1^2)+(heat_ratio-1)*(u2/u1)^3, heat_ratio*u3/u1-1.5*(heat_ratio-1)*(u2/u1)^2, u2/u1*heat_ratio];
+%     end
+end
+
+function flux = f(u, heat_ratio) %u[3x1]
+  flux = [u(2);
+          u(3)*(heat_ratio-1)+(3-heat_ratio)/2*(u(2).^2)./u(1);
+          u(2)*u(3)/u(1)*heat_ratio-0.5*(heat_ratio-1)*(u(3).^3)/(u(1).^2)];
+end
+
 time = 0; n = 1; T = [0];
 while(true) %iterate over time
     eigs = zeros(1,N);
     for i = 1:N
-        Ai = A(U(:,i,n));
-        eigs(i) = max(abs(eig(Ai)));
+        eigs(i) = max(abs(fast_eigs(U(:,i,n), Cv)));
     end
     dt = cfl_a*dx/max(eigs); %CFL condition
     if time > T_end
@@ -43,95 +88,35 @@ while(true) %iterate over time
     T = [T time];
     lambda = dt/dx;
     %calc first one
-    U(:,1,n+1) = U(:,1,n)-lambda*(F(U(:,1,n),U(:,2,n),lambda)-F(U(:,1,n),U(:,1,n),lambda));
+    U(:,1,n+1) = U(:,1,n)-lambda*(F_HLL(U(:,1,n),U(:,2,n),Cv)-F_HLL(U(:,1,n),U(:,1,n),Cv));
     %calc 2 thru N-1
     for j = 2:size(U,2)-1 %iterate over x
-        F_1 = F(U(:,j,n),U(:,j+1,n),lambda);
-        F_2 = F(U(:,j-1,n),U(:,j,n),lambda);
+        F_1 = F_HLL(U(:,j,n),U(:,j+1,n),Cv);
+        F_2 = F_HLL(U(:,j-1,n),U(:,j,n),Cv);
         U(:,j,n+1) = U(:,j,n)-lambda*(F_1-F_2);
     end
     %calc last one
     j = size(U,2);
-    U(:,j,n+1) = U(:,j,n)-lambda*(F(U(:,j,n),U(:,j,n),lambda)-F(U(:,j-1,n),U(:,j,n),lambda));
+    U(:,j,n+1) = U(:,j,n)-lambda*(F_HLL(U(:,j,n),U(:,j,n),Cv)-F_HLL(U(:,j-1,n),U(:,j,n),Cv));
     n = n+1;
 end
 dt = T_end-time;
-if dt > 0
-    %calc last time step
-    T = [T T_end];
-    lambda = dt/dx;
-    U(:,1,n+1) = U(:,1,n)-dt/dx*(F(U(:,1,n),U(:,2,n),lambda)-F(U(:,1,n),U(:,1,n),lambda));
-    %calc 2 thru N-1
-    for j = 2:size(U,2)-1 %iterate over x
-        F_1 = F(U(:,j,n),U(:,j+1,n),lambda);
-        F_2 = F(U(:,j-1,n),U(:,j,n),lambda);
-        U(:,j,n+1) = U(:,j,n)-dt/dx*(F_1-F_2);
-    end
-    %calc last one
-    j = size(U,2);
-    U(:,j,n+1) = U(:,j,n)-dt/dx*(F(U(:,j,n),U(:,j,n),lambda)-F(U(:,j-1,n),U(:,j,n),lambda));
-end
 
 figure(1)
-set(gcf,'position',[100 100 1000 600]);
 for t = 1:length(T)
-    tiledlayout(3,1);
-    nexttile
+    subplot(3,1,1);
     plot(x,U(1,:,t));
     xlabel("T = "+t*dt);
     title("Density");
-    nexttile
+    subplot(3,1,2)
     plot(x,U(2,:,t));
     xlabel("T = "+t*dt);
     title("Velocity");
-    nexttile
+    subplot(3,1,3)
     plot(x,U(3,:,t));
     xlabel("T = "+t*dt);
     title("Pressure");
     pause(0.005);
 end
 
-function F_LF = F_LF(uL,uR,lambda)
-    F_LF = 0.5*(A(uL)*uL + A(uR)*uR - (uR-uL)/lambda);
-end
-function FLF = F(uL,uR,lambda) %u[3x1]
-    FLF = zeros(3,1);
-    aL = min(eig(A(uL)),eig(A(uR)));
-    aR = max(abs(eig(A(uL))),abs(eig(A(uR))));
-    if aL >= 0
-        FLF = f(uL);
-    elseif (aL <= 0) & (aR >= 0)
-        FLF = 1./(aR-aL).*(aR.*f(uL)-aL.*f(uR)+aL.*aR.*(uR-uL));
-    elseif aR >= 0
-        FLF = f(uR);
-    end
-end
 
-function Au = A(u)
-    Cv = 1.4;
-    u1 = u(1);
-    u2 = u(2);
-    u3 = u(3);
-%     if (u1 == 0) % case divide by 0
-%         Au = [0, 1, 0;
-%              (Cv-3)/2*(u2)^2, (3-Cv)*(u2), Cv-1;
-%              -Cv*u2*u3+(Cv-1)*(u2)^3, Cv*u3-1.5*(Cv-1)*(u2)^2, u2*Cv];
-%     else
-        Au = [0, 1, 0;
-             (Cv-3)/2*(u2/u1)^2, (3-Cv)*(u2/u1), Cv-1;
-             -Cv*u2*u3/(u1^2)+(Cv-1)*(u2/u1)^3, Cv*u3/u1-1.5*(Cv-1)*(u2/u1)^2, u2/u1*Cv];
-%     end
-end
-
-function flux = f(u) %u[3x1]
-    Cv = 1.4;
-%     if u(1)==0 %case divide by 0
-%         flux = [u(2);
-%             u(3)*(Cv-1)+(3-Cv)/2*(u(2).^2);
-%             u(2).*u(3)*Cv-0.5*(Cv-1)*(u(3).^3)];
-%     else
-        flux = [u(2);
-            u(3)*(Cv-1)+(3-Cv)/2*(u(2).^2)./u(1);
-            u(2)*u(3)/u(1)*Cv-0.5*(Cv-1)*(u(3).^3)/(u(1).^2)];
-%     end
-end
