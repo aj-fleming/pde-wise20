49032445f4a59261825d80a98cab7a7bb39dd777
kenny-yung <kenny-yung@users.noreply.github.com>
2021-02-10 10:40:21 +0100

    made adjustments

--- prog4/prog4_main.m
+++ prog4/prog4_main.m
@@ -1,9 +1,9 @@
-N = 20; %grid cells
+N = 100; %grid cells
 x_start = 0;
 x_end = 1;
 T_end = 0.5;
 dx = (x_end-x_start)/N;
-cfl_a = 0.5;
+cfl_a = 0.75;
 Cv = 1.4;
 x = linspace(x_start,x_end,N);
 u0 = zeros(3,N); %rho,v,P
@@ -16,17 +16,64 @@ switch IC
         u0 = [1, -1, 0.4;
               1, 1, 0.4];
 end
-
+%functions for IC in U[rho; rho*v, E]
 U0 = cell(3,1);
 U0{1} = @(x) u0(1,1).* and(x>=0, x<=0.5) + u0(2,1).* and(x<=1, x>=0.5);
 U0{2} = @(x) u0(1,1)* u0(1,2).* and(x>=0, x<=0.5) + u0(2,1)*u0(2,2).* and(x<=1, x>=0.5);
 U0{3} = @(x) 0.5.*U0{1}(x).*U0{2}(x).^2 + u0(1,3)/(Cv-1).* and(x>=0, x<=0.5) + u0(2,3)/(Cv-1).* and(x<=1, x>=0.5);
-
+%calculate cell mean values for U[t=0]
 U = zeros(3,N,2);
 for i = 1:N
     U(1,i,1) =  integral(U0{1},x_start+(i-1)*dx, x_start+(i*dx))/dx;
     U(2,i,1) =  integral(U0{2},x_start+(i-1)*dx, x_start+(i*dx))/dx;
     U(3,i,1) =  integral(U0{3},x_start+(i-1)*dx, x_start+(i*dx))/dx;
+end
+
+time = 0; n = 1; T = [0];
+while(true) %iterate over time
+    eigs = zeros(1,N);
+    for i = 1:N
+        eigs(i) = max(abs(fast_eigs(U(:,i,n), Cv)));
+    end
+    dt = cfl_a*dx/max(eigs); %CFL condition
+    if time > T_end
+        break;
+    end
+    time = time + dt;
+    T = [T time];
+    lambda = dt/dx;
+    %calc first one
+    U(:,1,n+1) = U(:,1,n)-lambda*(F_HLL(U(:,1,n),U(:,2,n),Cv)-F_HLL(U(:,1,n),U(:,1,n),Cv));
+    %calc 2 thru N-1
+    for j = 2:size(U,2)-1 %iterate over x
+        F_1 = F_HLL(U(:,j,n),U(:,j+1,n),Cv);
+        F_2 = F_HLL(U(:,j-1,n),U(:,j,n),Cv);
+        U(:,j,n+1) = U(:,j,n)-lambda*(F_1-F_2);
+    end
+    %calc last one
+    j = size(U,2);
+    U(:,j,n+1) = U(:,j,n)-lambda*(F_HLL(U(:,j,n),U(:,j,n),Cv)-F_HLL(U(:,j-1,n),U(:,j,n),Cv));
+    n = n+1;
+end
+
+figure(1)
+set(gcf,'position',[100 100 1000 600]);
+for t = 1:length(T)
+    subplot(3,1,1);
+    plot(x,U(1,:,t));
+    ylim([0,1]);
+    xlabel("T = "+T(t));
+    title("Density");
+    subplot(3,1,2)
+    plot(x,U(2,:,t));
+    xlabel("T = "+T(t));
+    title("Velocity");
+    subplot(3,1,3)
+    plot(x,U(3,:,t));
+    %ylim([0,1.5]);
+    xlabel("T = "+T(t));
+    title("Pressure");
+    pause(0.0005);
 end
 
 function e = fast_eigs(u, heat_ratio)
@@ -57,7 +104,7 @@ function FLF = F_HLL(uL,uR,heat_ratio) %u[3x1]
         FLF = f(uR,heat_ratio);
     end
 end
-
+%calc jacobian
 function Au = A(u, heat_ratio)
     u1 = u(1);
     u2 = u(2);
@@ -65,58 +112,10 @@ function Au = A(u, heat_ratio)
     Au = [0, 1, 0;
           (heat_ratio-3)/2*(u2/u1)^2, (3-heat_ratio)*(u2/u1), heat_ratio-1;
           -heat_ratio*u2*u3/(u1^2)+(heat_ratio-1)*(u2/u1)^3, heat_ratio*u3/u1-1.5*(heat_ratio-1)*(u2/u1)^2, u2/u1*heat_ratio];
-%     end
 end
-
+%calc system F(U)
 function flux = f(u, heat_ratio) %u[3x1]
   flux = [u(2);
           u(3)*(heat_ratio-1)+(3-heat_ratio)/2*(u(2).^2)./u(1);
           u(2)*u(3)/u(1)*heat_ratio-0.5*(heat_ratio-1)*(u(3).^3)/(u(1).^2)];
 end
-
-time = 0; n = 1; T = [0];
-while(true) %iterate over time
-    eigs = zeros(1,N);
-    for i = 1:N
-        eigs(i) = max(abs(fast_eigs(U(:,i,n), Cv)));
-    end
-    dt = cfl_a*dx/max(eigs); %CFL condition
-    if time > T_end
-        break;
-    end
-    time = time + dt;
-    T = [T time];
-    lambda = dt/dx;
-    %calc first one
-    U(:,1,n+1) = U(:,1,n)-lambda*(F_HLL(U(:,1,n),U(:,2,n),Cv)-F_HLL(U(:,1,n),U(:,1,n),Cv));
-    %calc 2 thru N-1
-    for j = 2:size(U,2)-1 %iterate over x
-        F_1 = F_HLL(U(:,j,n),U(:,j+1,n),Cv);
-        F_2 = F_HLL(U(:,j-1,n),U(:,j,n),Cv);
-        U(:,j,n+1) = U(:,j,n)-lambda*(F_1-F_2);
-    end
-    %calc last one
-    j = size(U,2);
-    U(:,j,n+1) = U(:,j,n)-lambda*(F_HLL(U(:,j,n),U(:,j,n),Cv)-F_HLL(U(:,j-1,n),U(:,j,n),Cv));
-    n = n+1;
-end
-dt = T_end-time;
-
-figure(1)
-for t = 1:length(T)
-    subplot(3,1,1);
-    plot(x,U(1,:,t));
-    xlabel("T = "+t*dt);
-    title("Density");
-    subplot(3,1,2)
-    plot(x,U(2,:,t));
-    xlabel("T = "+t*dt);
-    title("Velocity");
-    subplot(3,1,3)
-    plot(x,U(3,:,t));
-    xlabel("T = "+t*dt);
-    title("Pressure");
-    pause(0.005);
-end
-
-
